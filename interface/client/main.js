aabc34b800b7e0393b261193a640f8acc2c7e5d7
Fabian Vogelsteller <fabian@frozeman.de>
2015-06-22 14:35:35 +0200

    made browser part of tabs

--- interface/client/main.js
+++ interface/client/main.js
@@ -2,3 +2,10 @@
 // set browser as default tab
 if(!LocalStore.get('selectedTab'))
     LocalStore.set('selectedTab', 'browser');
+
+if(!Tabs.findOne('browser'))
+    Tabs.insert({
+        _id: 'browser',
+        url: 'about:blank',
+        position: 0
+    });

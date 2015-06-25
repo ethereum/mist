67c2633edfab816514ca2d27f3364d4e0489007d
Fabian Vogelsteller <fabian@frozeman.de>
2015-06-25 11:32:00 +0200

    add admin api

--- modules/preloader/mistUI.js
+++ modules/preloader/mistUI.js
@@ -9,6 +9,9 @@ const MenuItem = remote.require('menu-item');
 const web3 = require('web3');
 const ipcProviderWrapper = require('../ipc/ipcProviderWrapper.js');
 
+const web3Admin = require('./web3Admin');
+web3Admin.extend(web3);
+
 // set web3 providor
 web3.setProvider(new web3.providers.IpcProvider('/tmp/geth.ipc', ipcProviderWrapper));
 

4640082e9477395a43f9cd00050af8c44c00063f
bas-vk <bas@ethdev.com>
2015-08-11 17:08:01 +0200

    fixed named pipe issue on windows

--- modules/ipc/getIpcPath.js
+++ modules/ipc/getIpcPath.js
@@ -12,7 +12,7 @@ module.exports = function() {
         path += '/.ethereum/geth.ipc';
 
     if(process.platform === 'win32')
-        path = p.join('\\\\?\\pipe', process.cwd(), 'geth.ipc');//'\\.\pipe\geth.ipc'; //global.path.APPDATA + '/Ethereum/geth.ipc';
+         path = '\\\\.\\pipe\\geth.ipc';
     
     console.log('CONNECT to IPC PATH: '+ path);
     return path;

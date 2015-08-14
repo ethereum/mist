fd7a8f28d4d38268645df1326ec670ba05fef941
Fabian Vogelsteller <fabian@frozeman.de>
2015-08-14 16:27:11 +0200

    switched to wallet mode

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ const syncMinimongo = require('./modules/syncMinimongo.js');
 
 // GLOBAL Variables
 global.production = false;
-global.mode = 'mist';
+global.mode = 'wallet';
 
 global.path = {
     HOME: app.getPath('home'),

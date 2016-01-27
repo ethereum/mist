3c48c42302a784d1b2250dff098b16c6d60d1119
Alexandre Van de Sande <alex.vandesande@ethdev.com>
2016-01-27 09:09:37 -0200

    Onboarding animations

--- interface/client/templates/popupWindows/onboardingScreen.js
+++ interface/client/templates/popupWindows/onboardingScreen.js
@@ -34,6 +34,37 @@ Template['popupWindows_onboardingScreen'].events({
    'click .goto-bitcoin': function(){
         TemplateVar.set('lastActive', TemplateVar.get('currentActive'));
         TemplateVar.set('currentActive','bitcoin');
-   }
+   },
+   'click .goto-tutorial-1': function(){
+        TemplateVar.set('lastActive', TemplateVar.get('currentActive'));
+        TemplateVar.set('currentActive','tutorial-1');
+   },
+   'click .goto-tutorial-2': function(){
+        TemplateVar.set('lastActive', TemplateVar.get('currentActive'));
+        TemplateVar.set('currentActive','tutorial-2');
+   },
+   /**
+    Request to create an account in mist
+    
+    @event click .create.account
+    */
+    'click .shapeshift': function(e){
+
+        // var shapeshiftWindow = new BrowserWindow({
+        //     width: 600 ,
+        //     height: 500,
+        //     icon: global.icon,
+        //     resizable: false,
+        //     'node-integration': false,
+        //     'standard-window': false,
+        //     'use-content-size': false,
+        //     frame: true,
+        //     'web-preferences': {
+        //         'web-security': false // necessary to make routing work on file:// protocol
+        //     }
+        // });
+        // shapeshiftWindow.loadURL('http://google.com');
+
+    }
 
 });

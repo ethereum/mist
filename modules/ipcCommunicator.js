f19815df8d4dc557749386d62c47af72f0af7406
Alexandre Van de Sande <alex.vandesande@ethdev.com>
2016-03-10 17:29:55 -0300

    Fixes #305 by adding one extra password verification step

--- modules/ipcCommunicator.js
+++ modules/ipcCommunicator.js
@@ -77,7 +77,7 @@ ipc.on('backendAction_importPresaleFile', function(e, path, pw) {
         if(data)
             console.log('Imported presale: ', data);
 
-        if(data.indexOf('Decryption failed:') !== -1) {
+        if(data.indexOf('Decryption failed:') !== -1 || data.indexOf('not equal to expected addr') !== -1) {
             e.sender.send('uiAction_importedPresaleFile', 'Decryption Failed');
 
         // if imported, return the address

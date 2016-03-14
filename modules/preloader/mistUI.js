3877c772062b6501f4e099c00d6b93926531a2b1
Fabian Vogelsteller <fabian@frozeman.de>
2016-03-14 16:45:15 +0100

    use fixed web3.js version in UI

--- modules/preloader/mistUI.js
+++ modules/preloader/mistUI.js
@@ -21,7 +21,10 @@ require('web-frame').setZoomLevelLimits(1, 1);
 // make variables globally accessable
 window.BigNumber = BigNumber;
 window.web3 = new Web3(new Web3.providers.IpcProvider('', ipcProviderWrapper));
-web3Admin.extend(window.web3);
+// add admin later
+setTimeout(function(){
+    web3Admin.extend(window.web3);
+}, 1000);
 
 window.dirname = __dirname;
 window.mist = mist();
@@ -29,6 +32,10 @@ window.syncMinimongo = syncMinimongo;
 window.ipc = ipc;
 window.platform = process.platform;
 
+
+// prevent overwriting the Dapps Web3
+delete global.Web3;
+delete window.Web3;
 
 
 // set the langauge for the electron interface

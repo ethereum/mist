713898b9b5e2193f37492dcc06353cf654d91e0a
V1C0D3R <V1C0D3R@gmail.com>
2016-04-03 16:06:21 +0200

    Add Mist's french i18n and add it to supported_languages in project-tap.i18n

--- modules/i18n.js
+++ modules/i18n.js
@@ -12,6 +12,7 @@ i18n.init({
         en: { translation: require('../interface/i18n/mist.en.i18n.json') },            
         de: { translation: require('../interface/i18n/mist.de.i18n.json') },            
         pt: { translation: require('../interface/i18n/mist.pt.i18n.json') },            
+        fr: { translation: require('../interface/i18n/mist.fr.i18n.json') },            
     }
 });
 

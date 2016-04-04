eb564c74f9bc8be5f7375544f6c6beac3ddfac14
Fabian Vogelsteller <fabian@frozeman.de>
2016-04-04 13:23:44 +0200

    fixed tapi18n module

--- modules/i18n.js
+++ modules/i18n.js
@@ -21,18 +21,8 @@ i18nConf.supported_languages.forEach(function(lang) {
 // init i18n
 i18n.init({
     lng: global.language || 'en',
-<<<<<<< HEAD
     resources: resources,
     interpolation: {prefix: '__', suffix: '__'}
-=======
-    resources: {
-        dev: { translation: require('../interface/i18n/mist.en.i18n.json') },
-        en: { translation: require('../interface/i18n/mist.en.i18n.json') },            
-        de: { translation: require('../interface/i18n/mist.de.i18n.json') },            
-        pt: { translation: require('../interface/i18n/mist.pt.i18n.json') },            
-        fr: { translation: require('../interface/i18n/mist.fr.i18n.json') },            
-    }
->>>>>>> develop
 });
 
 module.exports = i18n;

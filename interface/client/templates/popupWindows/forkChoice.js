2a73620adb7472ad85076df109e5fc7a9c4af184
Alexandre Van de Sande <alex.vandesande@ethdev.com>
2016-07-14 17:34:14 -0300

    added third option

---
+++ interface/client/templates/popupWindows/forkChoice.js
@@ -0,0 +1,12 @@
+
+Template['popupWindows_forkChoice'].onCreated(function(){
+
+
+})
+
+
+Template['popupWindows_forkChoice'].helpers({
+    'randomizer': function(){
+        return Math.random() > 0.5;
+    }
+})

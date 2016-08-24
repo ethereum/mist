a64d772dc21367e3d05bc36ea52b2f8a1fd25cfd
Everton Fraga <ev@ethereum.org>
2016-08-24 02:12:00 -0300

    Checking for R/W permissions

--- modules/db.js
+++ modules/db.js
@@ -15,8 +15,8 @@ exports.init = function() {
     return Q.try(() => {
         // if db file doesn't exist then create it
         try {
-            log.debug(`Check that db exists: ${filePath}`);
-            fs.accessSync(filePath, fs.W_OK);
+            log.debug('Check that db exists and it\'s writeable: ${filePath}');
+            fs.accessSync(filePath, fs.R_OK | fs.W_OK);
             return Q.resolve();
         } catch (err) {
             log.info(`Creating db: ${filePath}`);

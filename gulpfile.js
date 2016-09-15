f128f6eb5c31c0e2465549c1b27f196aa47cf429
Ramesh Nair <ram@hiddentao.com>
2016-09-15 13:27:53 +0800

    speed up signature download

--- gulpfile.js
+++ gulpfile.js
@@ -20,7 +20,7 @@ const mocha = require('gulp-spawn-mocha');
 var minimist = require('minimist');
 var fs = require('fs');
 var rcedit = require('rcedit');
-var syncRequest = require('sync-request');
+var got = require('got');
 
 var options = minimist(process.argv.slice(2), {
     string: ['platform','walletSource'],
@@ -523,27 +523,27 @@ gulp.task('getChecksums', [], function(done) {
 });
 
 
-gulp.task('download-signatures', function(){
-    var signatures = {},
-    getFrom4byteAPI = function(url){
-        console.log('Requesting ', url);
-        var res = syncRequest('GET', url);
-        if (res.statusCode == 200) {
-            var responseData = JSON.parse(res.getBody('utf8'));
-            _.map(responseData.results, function(e){
-                if (!!signatures[e.hex_signature]) {
-                    signatures[e.hex_signature].push(e.text_signature);
-                }
-                else {
-                    signatures[e.hex_signature] = [e.text_signature];
-                }
-            });
-            responseData.next && getFrom4byteAPI(responseData.next);
+gulp.task('download-signatures', function(cb) {
+    got('https://www.4byte.directory/api/v1/signatures/?page_size=20000&ordering=created_at', {
+        json: true
+    })
+    .then((res) => {
+        if (200 !== res.statusCode) {
+            throw new Error(res.statusText);
         }
-    };
-
-    getFrom4byteAPI('https://www.4byte.directory/api/v1/signatures/');
-    fs.writeFileSync('interface/client/lib/signatures.js', "window.SIGNATURES = " + JSON.stringify(signatures, null, '\t') + ";");
+        
+        let signatures = {};
+        
+        _.each(res.body.results, (e) => {
+            signatures[e.hex_signature] = signatures[e.hex_signature] || [];
+            signatures[e.hex_signature].push(e.text_signature);
+        });
+        
+        fs.writeFileSync('interface/client/lib/signatures.js', "window.SIGNATURES = " + JSON.stringify(signatures, null, '\t') + ";");
+        
+        cb();
+    })
+    .catch(cb);
 });
 
 

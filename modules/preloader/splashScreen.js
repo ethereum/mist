83295d96a3ee20a2b15d093c9dfd6dc8c4a30eef
Fabian Vogelsteller <fabian@frozeman.de>
2016-10-26 20:12:08 +0200

    fix vulns

--- modules/preloader/splashScreen.js
+++ modules/preloader/splashScreen.js
@@ -1,6 +1,7 @@
 require('./include/common')('splash');
 const mist = require('../mistAPI.js');
 const electron = require('electron');
+const remote = electron.remote;
 const ipc = electron.ipcRenderer;
 const ipcProviderWrapper = require('../ipc/ipcProviderWrapper.js');
 const Web3 = require('web3');
@@ -19,4 +20,5 @@ electron.webFrame.setZoomLevelLimits(1, 1);
 
 window.ipc = ipc;
 window.mist = mist();
+window.dirname = remote.getGlobal('dirname');
 window.web3 = new Web3(new Web3.providers.IpcProvider('', ipcProviderWrapper));

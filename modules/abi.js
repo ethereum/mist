9fb6750c0280c1eb48c742eb8b5c55208417a8c2
Ev <ev@ethereum.org>
2016-11-04 11:57:10 -0200

    [ESlint] Code fixes (#1368)
    * Code standards
    
    * Code standards
    
    * Spaces <> Tabs

--- modules/abi.js
+++ modules/abi.js
@@ -8,36 +8,36 @@ const ipc = electron.ipcMain;
 const abi = require('ethereumjs-abi');
 
 function isHexType(type) {
-	return _.includes(['address', 'bytes'], type) || type.match(/bytes\d+/g);
+    return _.includes(['address', 'bytes'], type) || type.match(/bytes\d+/g);
 }
 
-ipc.on('backendAction_decodeFunctionSignature', function(event, signature, data){
-	var dataBuffer, paramTypes;
-	dataBuffer = new Buffer(data.slice(10, data.length), 'hex');
-	signature = signature.match(/\((.+)\)/i);
+ipc.on('backendAction_decodeFunctionSignature', (event, signature, data) => {
+    var dataBuffer, paramTypes;
+    dataBuffer = new Buffer(data.slice(10, data.length), 'hex');
+    signature = signature.match(/\((.+)\)/i);
 
-	if (!signature) return;
+    if (!signature) return;
 
-	paramTypes = signature[1].split(',');
+    paramTypes = signature[1].split(',');
 
-	try	{
-		var paramsResponse = abi.rawDecode(paramTypes, dataBuffer);
-		var paramsDictArr = [];
+    try {
+        const paramsResponse = abi.rawDecode(paramTypes, dataBuffer);
+        var paramsDictArr = [];
 
-		// Turns addresses into proper hex string
-		// Turns numbers into their decimal string version
-		paramTypes.forEach((type, index) => {
-			var conversionFlag = isHexType(type) ? 'hex' : null,
-				prefix = isHexType(type) ? '0x' : '';
+        // Turns addresses into proper hex string
+        // Turns numbers into their decimal string version
+        paramTypes.forEach((type, index) => {
+            var conversionFlag = isHexType(type) ? 'hex' : null,
+                prefix = isHexType(type) ? '0x' : '';
 
-			paramsResponse[index] = paramsResponse[index].toString(conversionFlag);
+            paramsResponse[index] = paramsResponse[index].toString(conversionFlag);
 
-			paramsDictArr.push({type: type, value: prefix + paramsResponse[index]});
-		});
+            paramsDictArr.push({type: type, value: prefix + paramsResponse[index]});
+        });
 
-		event.sender.send('uiAction_decodedFunctionSignatures', paramsDictArr);
-	}
-	catch(e){
-		console.warn('ABI.js Warning:', e.message);
-	}
+        event.sender.send('uiAction_decodedFunctionSignatures', paramsDictArr);
+    }
+    catch(e){
+        console.warn('ABI.js Warning:', e.message);
+    }
 });

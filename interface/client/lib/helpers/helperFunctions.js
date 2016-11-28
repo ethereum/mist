f322f7f44982b2f497b51b8c17c869619ebf8dd6
Alex Van de Sande <avsa@ethereum.org>
2016-11-28 19:28:37 -0200

    remove duplicate function

--- interface/client/lib/helpers/helperFunctions.js
+++ interface/client/lib/helpers/helperFunctions.js
@@ -213,42 +213,6 @@ Helpers.detectNetwork = function (hash) {
     return network;
 };
 
-
-/**
-Detect Network
-
-@method detectNetwork
-**/
-Helpers.detectNetwork = function (hash) {
-    var network = {};
-
-    switch (hash) {
-    case '0xd4e56740f876aef8c010b86a40d5f56745a118d0906a34e69aec8c0db1cb8fa3':
-        console.log('Network is mainnet');
-        network.type = 'mainnet';
-        break;
-
-    case '0x41941023680923e0fe4d74a34bdac8141f2540e3ae90623718e47d66d1ca4a2d':
-        console.log('Network is testnet (Ropsten)');
-        network.type = 'testnet';
-        network.name = 'Ropsten';
-        break;
-
-    case '0x0cd786a2425d16f152c658316c423e6ce1181e15c3295826d7c9904cba9ce303':
-        console.log('Network is testnet (Morden)');
-        network.type = 'testnet';
-        network.name = 'Morden';
-        break;
-
-    default:
-        console.log('Network is privatenet');
-        network.type = 'privatenet';
-    }
-
-    return network;
-};
-
-
 /**
 Displays an error as global notification
 

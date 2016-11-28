953b6049d3df2e6b04a584b86b5ba570de975281
Alex Van de Sande <avsa@ethereum.org>
2016-11-28 14:42:56 -0200

    refactored into helper function

--- interface/client/templates/elements/nodeInfo.js
+++ interface/client/templates/elements/nodeInfo.js
@@ -48,19 +48,7 @@ Template['elements_nodeInfo'].onCreated(function(){
     // CHECK FOR NETWORK
     web3.eth.getBlock(0, function(e, res){
         if(!e){
-            switch(res.hash) {
-                case '0x0cd786a2425d16f152c658316c423e6ce1181e15c3295826d7c9904cba9ce303':
-                    TemplateVar.set(template, 'network', 'test'); // Morden
-                    break;
-                case '0x41941023680923e0fe4d74a34bdac8141f2540e3ae90623718e47d66d1ca4a2d':
-                    TemplateVar.set(template, 'network', 'test'); // Ropstein 
-                    break;
-                case '0xd4e56740f876aef8c010b86a40d5f56745a118d0906a34e69aec8c0db1cb8fa3':
-                    TemplateVar.set(template, 'network', 'main');
-                    break;
-                default:
-                    TemplateVar.set(template, 'network', 'private');
-            }
+            TemplateVar.set(template, 'network', Helpers.detectNetwork(res.hash).type); 
         }
     });
 

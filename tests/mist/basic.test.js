04510e5b6cf8edbc6cc45abd1ea51ff5ab899adb
Everton Fraga <ev@ethereum.org>
2017-01-17 18:11:07 -0200

    Tests for ETH-01-002

--- tests/mist/basic.test.js
+++ tests/mist/basic.test.js
@@ -123,7 +123,60 @@ test['Should select Wallet and Browse tabs properly'] = function* () {
 test['Check disallowed protocols'] = function* () {
     const client = this.client;
 
-    yield client.setValue('#url-input', 'javascript:window.close()');
-    yield client.submitForm('form.url');
-    yield Q.delay(1000);
+    yield this.loadFixture();
 };
+
+test['Check allowed "http://" protocol'] = function* () { // ETH-01-002
+    const client = this.client;
+    yield this.loadFixture();
+    yield client.setValue('#url-input', `${this.fixtureBaseUrl}index.html`);
+
+    const isProtocolBlocked = (yield client.execute(() => { // Code executed in context of browser
+        try { $('form.url').submit(); }
+        catch(e) { return /Invalid URL/.test(e); }
+        return false;
+    })).value;
+    console.log('isProtocolBlocked', isProtocolBlocked);
+
+    isProtocolBlocked.should.be.false;
+
+    const browserBarText = yield this.getBrowserBarText();
+    yield Q.delay(1000);
+    console.log('browserBarText', browserBarText);
+    browserBarText.should.eql(`http://localhost:8080 ▸ index.html`); // checks that did change displayed URL
+};
+
+test['Check disallowed "javascript:" protocol'] = function* () { // ETH-01-002
+    const client = this.client;
+    yield this.loadFixture();
+    yield client.setValue('#url-input', 'javascript:window.close()');
+
+    const isProtocolBlocked = (yield client.execute(() => { // Code executed in context of browser
+        try { $('form.url').submit(); }
+        catch(e) { return /Invalid URL/.test(e); }
+        return false;
+    })).value;
+    isProtocolBlocked.should.be.true;
+
+    const browserBarText = yield this.getBrowserBarText();
+    browserBarText.should.eql('http://localhost:8080'); // checks that hasn't changed displayed URL
+};
+
+test['Check disallowed "data:" protocol'] = function* () { // ETH-01-002
+    const client = this.client;
+    yield this.loadFixture();
+    yield client.setValue('#url-input', 'data:text/plain;charset=utf-8;base64,dGhpcyB0ZXN0IGlzIG9uIGZpcmU=');
+
+    const isProtocolBlocked = (yield client.execute(() => { // Code executed in context of browser
+        try { $('form.url').submit(); }
+        catch(e) { return /Invalid URL/.test(e); }
+        return false;
+    })).value;
+    isProtocolBlocked.should.be.true;
+
+    const browserBarText = yield this.getBrowserBarText();
+    browserBarText.should.eql('http://localhost:8080'); // checks that hasn't changed displayed URL
+};
+
+
+

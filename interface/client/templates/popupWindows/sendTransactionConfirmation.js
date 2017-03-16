1ffa3b95d0fff567512b2f8f3fcc244f5a2a2c36
Luca Zeug <luca@ethereum.org>
2017-03-16 17:28:11 +0100

    fix rule "spaced-comment"

--- interface/client/templates/popupWindows/sendTransactionConfirmation.js
+++ interface/client/templates/popupWindows/sendTransactionConfirmation.js
@@ -291,7 +291,7 @@ Template['popupWindows_sendTransactionConfirmation'].events({
     @event change .provided-gas, input .provided-gas
     */
     'change .provided-gas, input .provided-gas': function (e, template) {
-        var gas = template.$('.provided-gas').text().replace(/[, ]+/g,'');//template.$('.provided-gas').text();
+        var gas = template.$('.provided-gas').text().replace(/[, ]+/g,'');// template.$('.provided-gas').text();
 
         TemplateVar.set('providedGas', gas);
     },

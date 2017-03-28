f49a4122b1f777e380d290a049eb8493cade6329
Victor Hernandes Silva Maia <srvictormaia@gmail.com>
2017-03-28 18:11:25 -0300

    Remove outdated comments

--- main.js
+++ main.js
@@ -378,8 +378,6 @@ onReady = () => {
             return swarmNode.init();
         })
         .then(function sanityCheck() {
-            console.log("STARTED SWARM");
-
             if (!ethereumNode.isIpcConnected) {
                 throw new Error('Either the node didn\'t start or IPC socket failed to connect.');
             }

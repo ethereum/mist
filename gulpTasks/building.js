0b81be399287dd7ecd7192d976c2451ba514af31
cranelv <cranelv@163.com>
2017-11-09 17:15:20 +0800

    modify wallet path to wanchain

--- gulpTasks/building.js
+++ gulpTasks/building.js
@@ -91,9 +91,9 @@ gulp.task('bundling-interface', (cb) => {
             bundle(`&& cd ../../meteor-dapp-wallet/app \
                 && meteor-build-client ../../mist/dist_${type}/app/interface/wallet -p ""`);
         } else {
-            console.log(`Pulling https://github.com/ethereum/meteor-dapp-wallet/tree/${options.walletSource} "${options.walletSource}" branch...`);
+            console.log(`Pulling https://github.com/wanchain/meteor-dapp-wallet/tree/${options.walletSource} "${options.walletSource}" branch...`);
             bundle(`&& cd ../dist_${type} \
-                && git clone --depth 1 https://github.com/ethereum/meteor-dapp-wallet.git \
+                && git clone --depth 1 https://github.com/wanchain/meteor-dapp-wallet.git \
                 && cd meteor-dapp-wallet/app \
                 && meteor-build-client ../../app/interface/wallet -p "" \
                 && cd ../../ \

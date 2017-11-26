6a6d690c8956e754195ed8a2668b90ec80dbfd77
Zhang Lihua <zhang.lihua@wanglutech.com>
2017-11-26 16:29:17 +0800

    update scan

--- modules/wanChain/wanChainOTAs.js
+++ modules/wanChain/wanChainOTAs.js
@@ -13,7 +13,7 @@ OTAsCollection struct
 
 exports.getScanedByWaddr = function(waddr){
     let ScanBlockIndex = db.getCollection('ScanBlockIndex');
-    let Index = ScanBlockIndex.find({'address': waddr});
+    let Index = ScanBlockIndex.find({'_id': waddr});
     console.log("getScanedByWaddr:", Index);
     const begin = Index.length === 0 ? 0:Index[0].index;
     return begin;
@@ -21,7 +21,6 @@ exports.getScanedByWaddr = function(waddr){
 exports.setScanedByWaddr = function (waddr, scaned) {
     let ScanBlockIndex = db.getCollection('ScanBlockIndex');
     var found = ScanBlockIndex.findOne({'_id': waddr});
-    console.log("found:",found);
     if(found == null) {
         ScanBlockIndex.insert({
             _id: waddr,
@@ -52,10 +51,10 @@ exports.insertOtabyWaddr = function(waddr, ota, value, status,timeStamp) {
 
 exports.requireOTAsFromCollection = (waddr) =>
 {
-    console.log('scanOTAsByblocks:' + waddr);
+    console.log('requireOTAsFromCollection:' + waddr);
     var OTAsCollection = db.getCollection('OTAsCollection');
     var Key = waddr.toLowerCase();
-    console.log('scanOTAsByblocks:' + Key);
+    console.log('requireOTAsFromCollection:' + Key);
     return OTAsCollection.find({'address':Key});
 }
 exports.firstNewAccount = (newAccount) =>

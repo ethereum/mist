06717f63bd863d583761ed56cfbafd790498d4a9
Everton Fraga <ev@ethereum.org>
2017-11-28 15:10:43 -0500

    Returns original result as fallback

--- modules/ipc/methods/eth_getTransactionReceipt.js
+++ modules/ipc/methods/eth_getTransactionReceipt.js
@@ -4,6 +4,8 @@ const eth = require('ethereumjs-util');
 
 /**
  * Process method: eth_getTransactionReceipt
+ * Due to a geth's light client v1 bug, it does not return
+ * contractAddress value on the receipts. Let's fix that.
  */
 
 module.exports = class extends BaseProcessor {
@@ -20,41 +22,43 @@ module.exports = class extends BaseProcessor {
             fullResult: true
         });
 
-        // If that contains a contractAddress already, fine.
-        if (ret.result && ret.result.result.contractAddress != null) {
+        try {
+            // If that contains a contractAddress already, fine.
+            if (ret.result.result && ret.result.result.contractAddress != null) {
+                return ret.result;
+            }
+
+            // 1. GET TRANSACTION from AND nonce VALUES
+            const transactionInfo = await conn.socket.send({
+                jsonrpc: '2.0',
+                id: _.uuid(),
+                method: 'eth_getTransactionByHash',
+                params: [txHash]
+            }, { fullResult: true });
+
+            const fromAddress = transactionInfo.result.result.from;
+            const nonce = parseInt(transactionInfo.result.result.nonce, 16);
+            const possibleContractAddress = `0x${eth.generateAddress(fromAddress, nonce).toString('hex')}`;
+
+
+            // 2. GET CODE FROM ADDRESS
+            const contractCode = await conn.socket.send({
+                jsonrpc: '2.0',
+                id: _.uuid(),
+                method: 'eth_getCode',
+                params: [possibleContractAddress, 'latest']
+            }, { fullResult: true });
+
+            const contractCodeResult = contractCode.result.result;
+
+            // 3. IF IT EXISTS, ASSIGN TO RETURN VALUE
+            if (contractCodeResult && contractCodeResult.length > 2) {
+                ret.result.result.contractAddress = possibleContractAddress;
+            }
+        } catch (e) {
+            console.warn('[WARN]', txHash, e);
             return ret.result;
         }
-
-        // Due to a geth's light client v1 bug, it does not return
-        // contractAddress value on the receipts. Let's fix that.
-        // 1. GET TRANSACTION from AND nonce VALUES
-        const transactionInfo = await conn.socket.send({
-            jsonrpc: '2.0',
-            id: _.uuid(),
-            method: 'eth_getTransactionByHash',
-            params: [txHash]
-        }, { fullResult: true });
-
-
-        const fromAddress = transactionInfo.result.result.from;
-        const nonce = parseInt(transactionInfo.result.result.nonce, 16);
-        const possibleContractAddress = `0x${eth.generateAddress(fromAddress, nonce).toString('hex')}`;
-
-
-        // 2. GET CODE FROM ADDRESS
-        const contractCode = await conn.socket.send({
-            jsonrpc: '2.0',
-            id: _.uuid(),
-            method: 'eth_getCode',
-            params: [possibleContractAddress, 'latest']
-        }, { fullResult: true });
-        const contractCodeResult = contractCode.result.result;
-
-        // 3. IF IT EXISTS, ASSIGN TO RETURN VALUE
-        if (contractCodeResult && contractCodeResult.length > 2) {
-            ret.result.result.contractAddress = possibleContractAddress;
-        }
-
         return ret.result;
     }
 };

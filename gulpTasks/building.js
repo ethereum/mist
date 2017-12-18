d9cd3ab48bba7cd35dbb019f7be797bf95a32201
Zhang Lihua <zhang.lihua@wanglutech.com>
2017-12-18 10:48:23 +0800

    change name to WanWalletGui

--- gulpTasks/building.js
+++ gulpTasks/building.js
@@ -13,7 +13,7 @@ const version = require('../package.json').version;
 
 
 const type = options.type;
-const applicationName = (options.wallet) ? 'WanWallet' : 'Mist';
+const applicationName = (options.wallet) ? 'WanWalletGui' : 'Mist';
 
 
 gulp.task('clean-dist', (cb) => {

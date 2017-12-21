9ca6a39b783c20a725d8179703739b7d68ed95b5
astro9113 <astro913@163.com>
2017-12-21 18:46:23 +0800

    update explorer href

--- release_internal.sh
+++ release_internal.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-gulp --wallet
+gulp --wallet --internal
 cp /Users/wanchain/astro/jsPro/wanwallet/dist_wallet/release/* /Users/wanchain/Downloads/wanwallet_gui/
 
 rm -rf dist_wallet/

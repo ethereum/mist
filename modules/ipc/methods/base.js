b39fb97c62f4620ccad8e7403108a3eb281c6776
Marc Garreau <marcdgarreau@gmail.com>
2018-02-08 12:44:01 -0700

    WIP - throttling

--- modules/ipc/methods/base.js
+++ modules/ipc/methods/base.js
@@ -1,3 +1,4 @@
+import throttle from 'lodash/throttle';
 const _ = require('../../utils/underscore.js');
 const Q = require('bluebird');
 
@@ -18,7 +19,7 @@ module.exports = class BaseProcessor {
         this._ipcProviderBackend = ipcProviderBackend;
         this.ERRORS = this._ipcProviderBackend.ERRORS;
 
-        this.remoteIgnoreMethods = [    
+        this.remoteIgnoreMethods = [
             'net_peerCount',
             'eth_mining',
             'eth_accounts'
@@ -45,43 +46,54 @@ module.exports = class BaseProcessor {
      * @param  {Object|Array} payload  payload
      * @return {Promise}
      */
-    async exec(conn, payload) {
-        // console.log('$$$');
-        // console.log(payload);
-        // this._log.trace('Execute request', payload);
+    exec(conn, payload) {
+        this._log.trace('Execute request', payload);
 
         if (Array.isArray(payload)) {
-            const result = []; 
-            payload.forEach((value) => {
-                if (this.throttleMethods.includes(value.method)) {
-                    const requestSignature = this._requestSignature(value, conn);
-                    if (!this.throttles[requestSignature]) {
-                        this.throttles[requestSignature] = _.throttle(_.bind(this._sendPayload, this), 7500);
-                    }
-                    result.push(this.throttles[this._requestSignature(value, conn)](value, payload));
-                } else {
-                    result.push(this._sendPayload(value, conn));
-                }
-            });
-            const ret = await Promise.all(result);
-            console.log('&&&');
-            console.log(ret);
+            return this._handleArrayExec(conn, payload);
+        } else {
+            return this._handleObjectExec(conn, payload);
+        }
+    }
+
+    async _handleObjectExec(conn, payload) {
+        if (this.throttleMethods.includes(payload.method)) {
+            const requestSignature = this._requestSignature(payload, conn);
+            if (!this.throttles[requestSignature]) {
+                this.throttles[requestSignature] = throttle(_.bind(this._sendPayload, this), 5000);
+            }
+            const throttledResponse = await this.throttles[requestSignature](payload, conn);
+            const ret = Object.assign({}, throttledResponse, { id: payload.id });
             return ret;
         } else {
-            if (this.throttleMethods.includes(payload.method)) {
-                const requestSignature = this._requestSignature(payload, conn);
-                if (!this.throttles[requestSignature]) {
-                    this.throttles[requestSignature] = _.throttle(_.bind(this._sendPayload, this), 7500);
-                }
-                const throttle = await this.throttles[requestSignature](payload, conn);
-                const ret = Object.assign(throttle, {id: payload.id});
-                console.log('throttle');
-                console.log(ret);
-                return ret;
-            } else {
-                return await this._sendPayload(payload, conn);
-            }
+            return await this._sendPayload(payload, conn);
         }
+    }
+
+    async _handleArrayExec(conn, payload) {
+        const result = [];
+        const ids = payload.map(v => v.id);
+        console.log('∆∆∆ payload ids', ids);
+
+        payload.forEach((value) => {
+            if (this.throttleMethods.includes(value.method)) {
+                const requestSignature = this._requestSignature(value, conn);
+                if (!this.throttles[requestSignature]) {
+                    this.throttles[requestSignature] = throttle(_.bind(this._sendPayload, this), 5000);
+                }
+                result.push(this.throttles[this._requestSignature(value, conn)](value, payload));
+            } else {
+                result.push(this._sendPayload(value, conn));
+            }
+        });
+
+        const ret = await Promise.all(result);
+        const updatedRet = ret.map((result, i) => {
+            console.log('∆∆∆ SWAP-FROM!', result.id);
+            console.log('∆∆∆ SWAP-TO!', ids[i]);
+            return Object.assign({}, result, { id: ids[i] })
+        })
+        return updatedRet;
     }
 
     _requestSignature(payload, conn) {
@@ -103,33 +115,25 @@ module.exports = class BaseProcessor {
                 resolve(remoteResult);
             } else {
                 const ret = await conn.socket.send(payload, { fullResult: true });
-                console.log('***');
-                console.log(ret);
                 resolve(ret.result);
             }
         });
     }
 
     _shouldSendToRemote(payload, conn) {
-        const method = payload.method;
-
+        // Do NOT send to the remote node if: (all conditions must be satisfied)
+        // 1. the local node is synced
         const isRemote = store.getState().nodes.active === 'remote';
+        if (!isRemote) { return false; }
 
-        if (!isRemote) {
-            return false;
-        }
+        // 2. method is on the ignore list
+        const method = payload.method;
+        if (this.remoteIgnoreMethods.includes(method)) { return false; }
 
-        if (this.remoteIgnoreMethods.includes(method)) {
-            return false;
-        }
-
+        // 3. the method is 'eth_syncing' originating from the local node
         if (conn && conn.owner && conn.owner.history) {
-            console.log('###');
-            console.log(method);
-            console.log(conn.owner.history[0]);
-            console.log(payload.id);
             if (method === 'eth_syncing' && conn.owner.history[0] === 'http://localhost:3000/') {
-                console.log('@@@');
+                console.log('∆∆∆ local node sync call');
                 return false;
             }
         }
@@ -145,10 +149,7 @@ module.exports = class BaseProcessor {
                     reject(error);
                     return;
                 }
-
-                console.log('%%%');
-                console.log(result);
-
+                // console.log('∆∆∆ result from NodeRemote', result);
                 resolve(result);
             });
         });

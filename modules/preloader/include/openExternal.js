799e1acd515351defd6ee0b20cd60b9877342a7d
Alex Van de Sande <avsa@ethereum.org>
2018-03-07 07:58:05 -0300

    Removes popup code (#3691)
    * Removes popup code, adds changelly
    
    * revert changes to clientBinaryManager
    
    * removes unnecessary comment

--- modules/preloader/include/openExternal.js
+++ modules/preloader/include/openExternal.js
@@ -11,7 +11,6 @@ const { shell } = require('electron');
 document.addEventListener('click', (e) => {
     let node = false;
 
-
     if (e.target.nodeName === 'A') {
         node = e.target;
     } else if (e.target.parentNode && e.target.parentNode.nodeName === 'A') {

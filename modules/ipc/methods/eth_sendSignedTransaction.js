3eab2009456556c78eda14fac2a12c185b7ce69b
Ryan Ghods <ryan@ryanio.com>
2018-03-28 09:30:22 -0700

    Rename sendRawTransaction to sendSignedTransaction for web3.js 1.0

--- modules/ipc/methods/eth_sendSignedTransaction.js
+++ modules/ipc/methods/eth_sendSignedTransaction.js
@@ -9,13 +9,11 @@ module.exports = class extends BaseProcessor {
    * @override
    */
   async exec(conn, payload) {
-    console.log('∆∆∆ sendRaw payload', payload);
     if (store.getState().nodes.active === 'remote') {
       const result = await new Promise((resolve, reject) => {
         ethereumNodeRemote.web3.eth
           .sendSignedTransaction(payload.params[0])
           .once('transactionHash', hash => {
-            console.log('∆∆∆ hash', hash);
             resolve({
               jsonrpc: '2.0',
               id: payload.id,

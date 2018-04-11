8511efd80434ea4ba0a593a9c22edddbefbcb3d6
Ryan Ghods <ryan@ryanio.com>
2018-04-11 13:21:53 -0700

    Delay loading the webviews until node connection is established

--- interface/client/templates/layout/webviews.js
+++ interface/client/templates/layout/webviews.js
@@ -3,6 +3,19 @@ Template Controllers
 
 @module Templates
 */
+Template['layout_webviews'].onCreated(function() {
+  var template = this;
+  
+  TemplateVar.set(template, 'loading', true);
+
+  watchNodeStatus(template);
+});
+
+Template['layout_webviews'].onDestroyed(function() {
+  if (this.storeUnsubscribe) {
+    this.storeUnsubscribe();
+  }
+});
 
 /**
 The main section template
@@ -10,7 +23,6 @@ The main section template
 @class [template] layout_webviews
 @constructor
 */
-
 Template['layout_webviews'].helpers({
   /**
     Return the tabs
@@ -21,3 +33,19 @@ Template['layout_webviews'].helpers({
     return Tabs.find({}, { field: { position: 1 } });
   }
 });
+
+/**
+Set TemplateVar 'loading' whether node connection is active
+@method watchNodeStatus
+*/
+var watchNodeStatus = function(template) {
+    if (store.getState().nodes.remote.blockNumber > 1000 || store.getState().nodes.local.blockNumber > 1000) {
+      TemplateVar.set(template, 'loading', false);
+    }
+
+    this.storeUnsubscribe = store.subscribe(() => {
+      if (store.getState().nodes.remote.blockNumber > 1000 || store.getState().nodes.local.blockNumber > 1000) {
+        TemplateVar.set(template, 'loading', false);
+      }
+    });
+}

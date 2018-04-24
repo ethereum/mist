bbe53c6d54c12695e37318837b20b3f09fca9585
Ryan Ghods <ryan@ryanio.com>
2018-04-24 12:05:38 -0700

    Show loading first in wallet mode while node is connecting

--- main.js
+++ main.js
@@ -400,7 +400,27 @@ function initializeMainWindowListeners() {
     mainWindow.show();
   });
 
-  mainWindow.load(global.interfaceAppUrl);
+  // If in wallet mode, first show loading window
+  // then load wallet url once node connection is established.
+  // Otherwise, load immediately since we already
+  // have this logic in Mist in webviews.html
+  if (global.mode !== 'wallet') {
+    mainWindow.load(global.interfaceAppUrl);
+  } else {
+    mainWindow.load(
+      'data:text/html,<div class="loadingspinner"></div><style>body{background: #f1f1f1;height:100vh;margin: 0;padding: 0;display: flex;justify-content: center;align-items: center;}.loadingspinner{pointer-events: none;width: 3em;height: 3em;border: 0.4em solid transparent;border-color: #eee;border-top-color: #3E67EC;border-radius: 50%;animation: loadingspin 1s linear infinite;}@keyframes loadingspin{100% {transform: rotate(360deg)}</style>'
+    );
+    const unsubscribe = store.subscribe(() => {
+      if (
+        store.getState().nodes.remote.blockNumber > 1000 ||
+        store.getState().nodes.local.blockNumber > 1000
+      ) {
+        // Connected to node!
+        mainWindow.load(global.interfaceAppUrl);
+        unsubscribe();
+      }
+    });
+  }
 
   mainWindow.on('closed', () => store.dispatch(quitApp()));
 }

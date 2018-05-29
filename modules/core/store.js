21f34d212a57890d82fe5334b2994c5b69a6a301
Marc Garreau <marcdgarreau@gmail.com>
2018-05-29 15:43:58 -0600

    Introduces local debug server

--- modules/core/store.js
+++ modules/core/store.js
@@ -6,13 +6,19 @@ import { app } from 'electron';
 import rootReducer from './rootReducer';
 
 export default function configureReduxStore() {
+  // In development, send Redux actions to a local DevTools server
+  // Note: run and view these DevTools with `yarn dev:tools`
+  let debugWrapper = data => data;
+  if (process.env.NODE_ENV === 'development') {
+    debugWrapper = composeWithDevTools({ realtime: true, port: 8000 });
+  }
+
   const store = createStore(
     rootReducer,
-    composeWithDevTools(applyMiddleware(thunk, forwardToRenderer))
+    debugWrapper(applyMiddleware(thunk, forwardToRenderer))
   );
 
   replayActionMain(store);
 
   return store;
 }
-

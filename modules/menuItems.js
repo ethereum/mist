a686ab9a0811ba833968f531bfab9c62ea04cbf5
Marc Garreau <marcdgarreau@gmail.com>
2018-06-19 14:22:07 -0600

    Fixes disabled menu buttons

--- modules/menuItems.js
+++ modules/menuItems.js
@@ -599,10 +599,7 @@ let menuTempl = function(webviews) {
     submenu: [
       {
         label: i18n.t('mist.applicationMenu.develop.syncModeLight'),
-        enabled:
-          ethereumNode.isOwnNode &&
-          ethereumNode.isGeth &&
-          !ethereumNode.isDevNetwork,
+        enabled: ethereumNode.isOwnNode && !ethereumNode.isDevNetwork,
         checked: store.getState().nodes.local.syncMode === 'light',
         type: 'checkbox',
         click() {
@@ -629,10 +626,7 @@ let menuTempl = function(webviews) {
       },
       {
         label: i18n.t('mist.applicationMenu.develop.syncModeNoSync'),
-        enabled:
-          ethereumNode.isOwnNode &&
-          ethereumNode.isGeth &&
-          !ethereumNode.isDevNetwork,
+        enabled: ethereumNode.isOwnNode && !ethereumNode.isDevNetwork,
         checked: store.getState().nodes.local.syncMode === 'nosync',
         type: 'checkbox',
         click() {

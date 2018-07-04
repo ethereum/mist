f90df79fe42254ad0d6197df4a112dab259aa5c5
Everton Fraga <ev@ethereum.org>
2018-07-03 23:45:59 -0300

    Config: using yarn to run scripts

--- gulpTasks/building.js
+++ gulpTasks/building.js
@@ -81,7 +81,7 @@ gulp.task('pack-wallet', cb => {
       exec(
         `git clone --depth 1 https://github.com/ethereum/meteor-dapp-wallet.git && \
             cd meteor-dapp-wallet/app && \
-            npm install && \
+            yarn & \
             ../../node_modules/.bin/meteor-build-client ../../wallet -p ""`,
         (err, stdout, stderr) => {
           console.log(stdout, stderr);
@@ -113,7 +113,7 @@ gulp.task('bundling-interface', cb => {
     if (options.walletSource === 'local') {
       console.log('Use local wallet at ../meteor-dapp-wallet/app');
       bundle(`&& cd ../../meteor-dapp-wallet/app \
-              && npm install \
+              && yarn \
               && ../../../node_modules/.bin/meteor-build-client ../../mist/dist_${type}/app/interface/wallet -p ""`);
     } else {
       console.log(
@@ -124,7 +124,7 @@ gulp.task('bundling-interface', cb => {
       bundle(`&& cd ../dist_${type} \
                 && git clone --depth 1 https://github.com/ethereum/meteor-dapp-wallet.git \
                 && cd meteor-dapp-wallet/app \
-                && npm install \
+                && yarn \
                 && ../../../node_modules/.bin/meteor-build-client ../../app/interface/wallet -p "" \
                 && cd ../../ \
                 && rm -rf meteor-dapp-wallet`);

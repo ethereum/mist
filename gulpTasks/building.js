baf13271cda871b252cbba04f8c2579baf3dc5bf
Everton Fraga <ev@ethereum.org>
2018-07-31 11:10:29 -0300

    tweaking path name

--- gulpTasks/building.js
+++ gulpTasks/building.js
@@ -101,7 +101,7 @@ gulp.task('move-wallet', cb => {
 });
 
 gulp.task('build-interface', cb => {
-  const interfaceBuildPath = path.resolve('.build/interface');
+  const interfaceBuildPath = path.resolve('build-interface');
   exec(
     `yarn run meteor-build-client ${interfaceBuildPath} -p ""`,
     { cwd: 'interface' },
@@ -114,7 +114,7 @@ gulp.task('build-interface', cb => {
 
 gulp.task('copy-interface', () => {
   return gulp
-    .src(['.build/interface/*'])
+    .src(['build-interface/*'])
     .pipe(gulp.dest(`dist_${type}/app/interface`));
 });
 
